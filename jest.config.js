beea12e1ac41c96419be3d660fd1b7f342f040f8
Raj Mazumder <rajmazumder27.08.2001@gmail.com>
2024-11-21 22:40:16 +0530

    fix(worker): fix max testing worker to 1 to stop run test parallely

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 export default {
+  maxWorkers: 1,
   verbose: true,
   testEnvironment: "node",
   transform: {
@@ -8,5 +9,5 @@ export default {
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
   },
-  testMatch: ["**/*.spec.ts", "**/*.test.ts"]
+  testMatch: ["**/*.spec.ts", "**/*.test.ts"],
 };
